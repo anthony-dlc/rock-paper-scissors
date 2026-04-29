64ccedb97e97b12780deb0e542a3f1e0edc8b56e
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-04-29 15:46:48 -0400

    Fix rock-paper-scissors game and start again

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,55 +1,27 @@
-// "Rock Paper Scissors"
+// Getting computer choice:
 
 /*
-Inicio
-Computadora elije un numero 1 - 3
-Si computadora es igual 1
-   Presenta piedra
-Si computadora es igual 2
-   Presenta papel
-Sino presenta tijeras
-Fin
+Start
+Computer choice a number 1-3 randomly
+If computer choice is 1
+  return rock
+If computer choice is 2
+  return paper
+If computer choice is 3
+  return scissors
+EndIf
+End
 */
 
-const ROCK = "rock";
-const PAPER = "paper"
-const SCISSORS = "scissors"
-
-function getComputerchoice(){
- let computer = Math.floor(Math.random() * 3) + 1;
-  if (computer === 1){
-    console.log(ROCK)
-  } else if (computer === 2){
-    console.log(PAPER)
-  } else {
-    console.log(SCISSORS)
+function getComputerChoice(){
+  const computerChoice = Math.floor(Math.random() * 3) + 1;
+  if (computerChoice === 1){
+    return "rock"
+  } else if(computerChoice === 2){
+    return "paper"
+  } else{
+    return "scissor"
   }
 }
 
-// getComputerchoice()
-
-/*
-Se pude al humano entrar una opcion
-Esa opcion se convierte en minuscula
-Si el humano introdujo roca
-  Se muestra rock
-Si el humano introdujo papel
-  Se muestra papel
-Sino Se muestra tijeras
-*/
-
-function getHumaChoice(){
-  let human = prompt(`
-    Elije una opcion:
-   Rock - Paper - Scissors`);
-   let lower = human.toLowerCase();
-   if (lower === "rock"){
-    console.log(ROCK)
-   } else if (lower === "paper") {
-    console.log(PAPER)
-   } else {
-    console.log(SCISSORS)
-   }
-}
-
-getHumaChoice()
+// console.log(getComputerChoice())
