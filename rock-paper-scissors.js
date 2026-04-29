a236e3cb6d2c4fcc6f2f88c7253c565073549777
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-04-29 16:13:27 -0400

    Add play round to test how the game is going

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,6 +13,10 @@ EndIf
 End
 */
 
+//score:
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
   const computerChoice = Math.floor(Math.random() * 3) + 1;
   if (computerChoice === 1){
@@ -45,4 +49,53 @@ function getHumanChoice(){
   return lower
 }
 
-console.log(getHumanChoice())
+// console.log(getHumanChoice())
+
+// Play Round:
+
+/*
+Get human choice and computer choice
+compare human choice and computer choice
+add point who win
+show result
+*/
+
+function playRound(humanChoice,computerChoice){
+  // Human win
+  if (humanChoice === "rock" && computerChoice === "scissor"){
+    humanScore++
+    console.log(`${humanChoice} beats ${computerChoice}`)
+  } else if(humanChoice === "scissor" && computerChoice === "paper"){
+    humanScore++
+    console.log(`${humanChoice} beats ${computerChoice}`)
+  } else if(humanChoice === "paper" && computerChoice === "rock"){
+    humanScore++
+    console.log(`${humanChoice} beats ${computerChoice}`)
+  }
+  
+  // computer win
+  
+  else if (computerChoice === "rock" && humanChoice === "scissor"){
+    computerScore++
+    console.log(`${computerChoice} beats ${humanChoice}`)
+  } else if(computerChoice === "scissor" && humanChoice === "paper"){
+    computerScore++
+    console.log(`${computerChoice} beats ${humanChoice}`)
+  } else if(computerChoice === "paper" && humanChoice === "rock"){
+    computerScore++
+    console.log(`${computerChoice} beats ${humanChoice}`)
+  }
+
+  // equal
+  else{
+    console.log(`${humanChoice} and ${computerChoice} are the same, no one has a point`)
+  }
+  // console.log(`
+  //   Points: 
+  //   Human: ${humanScore}
+  //   Computer: ${computerScore}`)
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection)
