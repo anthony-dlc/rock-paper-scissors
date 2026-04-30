86878396adc65b2549b97b153e72e76a804e8b64
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-04-30 14:03:29 -0400

    Add function for human choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,3 +12,15 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice())
+
+// Get human choice:
+
+function getHumanChoice(){
+  const humanChoice = prompt(`
+    Write down one option:
+    Rock, Paper, Scissor`)
+  const lowerHumanChoice = humanChoice.toLowerCase()
+  return lowerHumanChoice
+}
+
+console.log(getHumanChoice())
