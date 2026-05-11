b68eb059bbc7e77da690504d1d84e3e9d1bd0182
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-05-11 19:28:54 -0400

    Add play round

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -9,53 +9,95 @@
 
 // hay una funcion que compara mi eleccion contra la de la computadore, agrega el punto ganador y muestra el resultado
 
-// HUMAN CHOICE: 
+// HUMAN CHOICE:
 
-const container = document.querySelector(".container")
-const rock = document.createElement("button")
-rock.textContent = "ROCK"
-rock.value = "rock"
+const container = document.querySelector(".container");
+const rock = document.createElement("button");
+rock.textContent = "ROCK";
+rock.value = "rock";
 
-const paper = document.createElement("button")
-paper.textContent = "PAPER"
-paper.value = "paper"
+const paper = document.createElement("button");
+paper.textContent = "PAPER";
+paper.value = "paper";
 
-const scissor = document.createElement("button")
-scissor.textContent = "SCISSOR"
-scissor.value = "scissor"
+const scissor = document.createElement("button");
+scissor.textContent = "SCISSOR";
+scissor.value = "scissor";
 
-container.appendChild(rock)
-container.appendChild(paper)
-container.appendChild(scissor)
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissor);
 
 // COMPUTER CHOICE:
 
-function getComputerChoice(){
-  let computerChoice = Math.floor(Math.random() * 3) + 1
-  if (computerChoice === 1){
-    return "rock"
-  } else if (computerChoice === 2){
-    return "paper"
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
+  if (computerChoice === 1) {
+    return "rock";
+  } else if (computerChoice === 2) {
+    return "paper";
   } else {
-    return "scissor"
+    return "scissor";
   }
 }
 
+// SCORE:
 
-// Score
+let hScore = 0;
+let cScore = 0;
 
-let hScore = 0
-let cScore = 0
+const scoreBox = document.querySelector(".score");
+const selections = document.createElement("p");
+const humanScore = document.createElement("p");
+const computerScore = document.createElement("p");
+humanScore.textContent = `Human score: ${hScore}`;
+computerScore.textContent = `Computer Score: ${cScore}`;
 
-const scoreBox = document.querySelector(".score")
-const selections = document.createElement("p")
-const humanScore = document.createElement("p")
-const computerScore = document.createElement("p")
-humanScore.textContent = `Human score: ${hScore}`
-computerScore.textContent = `Computer Score: ${cScore}`
+selections.textContent = "Selections";
 
-selections.textContent = "Selections"
+scoreBox.appendChild(selections);
+scoreBox.appendChild(humanScore);
+scoreBox.appendChild(computerScore);
 
-scoreBox.appendChild(selections)
-scoreBox.appendChild(humanScore)
-scoreBox.appendChild(computerScore)
+// PLAYROUND:
+
+function playRound(humanChoice, computerChoice) {
+  // HUMAN WINS:
+  if (humanChoice === "rock" && computerChoice === "scissor") {
+    hScore++;
+    selections.textContent = `Human: ${humanChoice} beats Computer: ${computerChoice}`;
+    humanScore.textContent = `Human score: ${hScore}`;
+    computerScore.textContent = `Computer Score: ${cScore}`;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    hScore++;
+    selections.textContent = `Human: ${humanChoice} beats Computer: ${computerChoice}`;
+    humanScore.textContent = `Human score: ${hScore}`;
+    computerScore.textContent = `Computer Score: ${cScore}`;
+  } else if (humanChoice === "scissor" && computerChoice === "paper") {
+    hScore++;
+    selections.textContent = `Human: ${humanChoice} beats Computer: ${computerChoice}`;
+    humanScore.textContent = `Human score: ${hScore}`;
+    computerScore.textContent = `Computer Score: ${cScore}`;
+  }
+
+  // COMPUTER WINS:
+
+  else if(computerChoice === "rock" && humanChoice === "scissor") {
+    cScore++;
+    selections.textContent = `Computer: ${computerChoice} beats Human: ${humanChoice}`;
+    humanScore.textContent = `Human score: ${hScore}`;
+    computerScore.textContent = `Computer Score: ${cScore}`;
+  } else if(computerChoice === "paper" && humanChoice === "rock") {
+    cScore++;
+    selections.textContent = `Computer: ${computerChoice} beats Human: ${humanChoice}`;
+    humanScore.textContent = `Human score: ${hScore}`;
+    computerScore.textContent = `Computer Score: ${cScore}`;
+  } else if(computerChoice === "scissor" && humanChoice === "paper") {
+    cScore++;
+    selections.textContent = `Computer: ${computerChoice} beats Human: ${humanChoice}`;
+    humanScore.textContent = `Human score: ${hScore}`;
+    computerScore.textContent = `Computer Score: ${cScore}`;
+  } else {
+    selections.textContent = `Human: ${humanChoice} & Computer: ${computerChoice} are the same`
+  }
+}
